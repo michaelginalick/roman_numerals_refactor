d8dbec55ed6ce520adf32725f74a70b1a2b64aa9
Michael <Michael.ginalick@gmail.com>
2014-11-25 21:21:20 -0600

    new files

---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,32 @@
+require 'roman_numerals'
+
+describe RomanNumerals do
+
+	it "should be able to convert 1 ro I" do
+		expect(RomanNumerals.convert(1)).to eq("I")
+	end 
+
+	it "should be able to convert 5 to V" do
+		expect(RomanNumerals.convert(5)).to eq("V")
+	end
+
+	it "should be able to convert 10 to X" do 
+		expect(RomanNumerals.convert(10)).to eq("X")
+	end
+
+	it "should be able to convert 35 to XXXV" do
+		expect(RomanNumerals.convert(35)).to eq("XXXV")
+	end
+
+	it "should be able to convert 996 to CMXCVI" do 
+		expect(RomanNumerals.convert(996)).to eq("CMXCVI")
+	end
+
+	it "should be able to convert 44 to XLIV" do 
+		expect(RomanNumerals.convert(44)).to eq("XLIV")
+	end
+
+	it "should be able to convert 1999 to MCMXCIX" do
+		expect(RomanNumerals.convert(1999)).to eq("MCMXCIX")
+	end
+end
